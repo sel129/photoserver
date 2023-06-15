f54f5ce25c0a7a94c53967f77eab3b436c02bfb6
Scott Lis <scott.lis@gmail.com>
2023-06-14 21:07:34 -0600

    add get photos apis

--- index.js
+++ index.js
@@ -1,25 +1,75 @@
 const express = require('express')
 const multer  = require('multer')
+const { Level } = require('level')
 const upload = multer({ dest: 'uploads/' })
+const db = new Level('uploads/photodb', { valueEncoding: 'json' })
 const port = 3000;
 
 const app = express()
 
-app.post('/profile', upload.single('avatar'), function (req, res, next) {
+// initialize the index
+async function initialize() {
+  await db.get('index', (err) => {
+    if(err?.code === 'LEVEL_NOT_FOUND') {
+      db.put('index', 0)
+    }
+  })
+}
+initialize();
+
+app.use(express.static('uploads'))
+
+app.post('/upload', upload.single('photo'), async function (req, res, next) {
   // req.file is the `avatar` file
   // req.body will hold the text fields, if there were any
   console.log(req.file)
+  const record = {
+    date: new Date(),
+    path: req.file.path.split('/')[1]
+  }
+  const index = await db.get('index')
+  await db.put(index, record)
+  await db.put('index', index + 1)
+
+  res.status(200).send('success')
   next();
 })
+
+app.get('/photo/:photoId', async (req, res) => {
+  const value = await db.get(req.params.photoId)
+  res.status(200).json(value)
+})
+
 
 app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
   // req.files is array of `photos` files
   // req.body will contain the text fields, if there were any
 })
 
-app.get('/app', (req, res) => {
-    res.send('<form action="/profile" method="post" enctype="multipart/form-data"><input type="file" name="avatar" /><input type="submit"></form>')
+app.get('/photos', async function(req, res, next) {
+  const from = Number(req.query.from)
+  const to = Number(req.query.to)
+
+  if(from > to) {
+    res.status(500).send("from > to")
+  }
+
+  const keys = [];
+  for (let i = from; i <= to; i++) {
+    keys.push(i)
+  }
+
+  const files = await db.getMany(keys)
+
+  res.status(200).json(files);
+
 })
+
+app.get('/app', (req, res) => {
+    res.send('<form action="/upload" method="post" enctype="multipart/form-data"><input type="file" name="photo" /><input type="submit"></form>')
+})
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
